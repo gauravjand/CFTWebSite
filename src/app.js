1a7c31902ec0c7c16112fdf6f13705dad3a02a05
gauravjand <31872205+gauravjand@users.noreply.github.com>
2020-03-20 23:32:40 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
-
+var {getCFTDetails}=require("./getAWSDetails.js")
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -37,13 +37,11 @@ res.render('CFTDetails')
 
 // Access the parse results as request.body
 app.post('/CFTDetails', function(request, response){
-
+    console.log("returned Value: ", getCFTDetails(request.body.txtRoleName))
     response.render('CFTDetails')
-
-
 });
 
 
-app.listen(80, () => {
+app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
---
+++ src/getAWSDetails.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+
+
+var AWS = require('aws-sdk');
+
+const getCFTDetails=function(roleName){
+
+
+// Set the region 
+AWS.config.update({region: 'us-east-2'});
+
+var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15'});
+
+  var params = {
+
+    PhysicalResourceId: 'AdvStackSecurityGroup2'
+
+  };
+  cloudformation.describeStackResources(params, function(err, data) {
+    if (err) return "No stack found for the role " + roleName
+    else     return data.StackResources[0].StackName;           // successful response
+  });
+
+}
+ 
+module.exports={getCFTDetails};
