9ca5a50d77b7fcbb98a6fa45ccb99ccb5172a854
gauravjand <31872205+gauravjand@users.noreply.github.com>
2020-03-23 17:15:33 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
-var {getCFTDetails}=require("./getAWSDetails.js")
+var AWS = require('aws-sdk');
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -33,15 +33,35 @@ app.get('/CFTDetails',async (req, res) => {
 
 res.render('CFTDetails')
 })
+app.get('/',async (req, res) => {
 
+    res.render('CFTDetails')
+    })
+    
 
 // Access the parse results as request.body
 app.post('/CFTDetails', function(request, response){
-    console.log("returned Value: ", getCFTDetails(request.body.txtRoleName))
-    response.render('CFTDetails')
+
+AWS.config.update({region: 'us-east-2'});    
+var cloudformation = new AWS.CloudFormation({apiVersion: '2010-09-09'});
+var params = {
+    PhysicalResourceId: request.body.txtRoleName
+  };
+
+
+
+  cloudformation.describeStackResources(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+        console.log(data.StackResources[0].StackName)
+        response.render("CFTName", {CFT_Name: data.StackResources[0].StackName})
+     }
+  });
 });
 
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
+
+
